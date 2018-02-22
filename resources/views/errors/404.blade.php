147fbf4fdb5ace1179404e6809c89c10a4b5f3d3
gscsdlz <lz842063523@foxmail.com>
2018-02-22 09:34:19 +0800

    修正404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,8 @@
-<h1>权限不足</h1>
+@extends('layout')
+@section('main')
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3" style="margin-top: 5%">
+            <h1 class="text-center">权限不足</h1>
+        </div>
+    </div>
+@endsection
