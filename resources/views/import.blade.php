611218a62c7973f706d4725fa5d0fde4f923fb17
gscsdlz <lz842063523@foxmail.com>
2018-02-22 17:10:15 +0800

    修改导入导出权限

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -4,12 +4,18 @@
     <div class="col-md-6 col-md-offset-3">
         <h1>导入导出</h1>
         <hr/>
+        @if(Session::get('privilege') == 1)
         <button class="btn btn-primary" type="button">导入员工数据</button>
         <button class="btn btn-primary" type="button">导入项目数据</button>
+        @endif
         <button class="btn btn-primary" type="button">导入进度数据</button>
+
         <hr/>
+
+        @if(Session::get('privilege') == 1)
         <button class="btn btn-default" type="button" onclick="window.location.href='{{ URL("export/people") }}'">下载员工数据</button>
         <button class="btn btn-default" type="button">下载项目数据</button>
+        @endif
         <button class="btn btn-default" type="button" onclick="window.location.href='{{ URL("export/search?project_id=0&member_id=0&stime=0&etime=0") }}'">下载进度数据</button>
         <hr/>
     </div>
